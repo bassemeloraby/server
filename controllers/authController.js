b6d02360b8ff6a16011b9a900b26f88ed006a17a
bassem <basem12683@gmail.com>
2025-03-08 00:41:26 +0300

    loginUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,3 +26,39 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }
 };
+
+// Login user
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      username: user.username,
+      role: user.role,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
